e53302b91d4e8268be632238f60e76bc6c37c35a
PUET1 <PUET1@10.129.25.62>
2018-08-06 10:49:49 +0200

    currency validator util added

---
+++ src/main/java/utils/CurrencyValidator.java
@@ -0,0 +1,20 @@
+package utils;
+import java.util.Currency;
+
+public class CurrencyValidator {
+
+	private void validateCurrency() {
+		try {
+			Currency.getInstance("EUY");
+			System.out.println("expected currency : "  );
+		} catch (Exception e) {
+			System.out.println("not a valid currency");
+		}
+	}
+	
+	public static void main(String args[]) {
+		CurrencyValidator cv = new CurrencyValidator();
+		cv.validateCurrency();
+	}
+	
+}
